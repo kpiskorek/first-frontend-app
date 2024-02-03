095d88cb128d7707949025dc7a8341b452dd3f45
kpiskorek <piskorekk@gmail.com>
2024-02-03 14:27:44 +0100

    build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/first-frontend-app",
+  build: { outDir: "docs" }
 })
